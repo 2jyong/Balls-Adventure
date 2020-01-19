3c8ee2e25a45e2e3beb721fda2028bf6555690c8
2jyong <03jjjang@naver.com>
2020-01-19 17:48:59 +0900

    Unity Balls Adventure_01

--- Assets/Scripts/RotateModuel.cs
+++ Assets/Scripts/RotateModuel.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 
 class RotateModuel : MonoBehaviour
 {
+    // 1.   행성계 구현하기
+    // 1++  행성계 곡선으로 구현하기
+    // 2.   isRotate 구현하기
+
     public float speed = 10;
     [SerializeField] private bool isRotate = false;
 
