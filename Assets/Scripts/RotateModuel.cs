e03f76b2b7c623a506f6ca9d2365ad6f7dc8e11f
2jyong <03jjjang@naver.com>
2020-01-19 17:45:16 +0900

    Unity Tutorial

---
+++ Assets/Scripts/RotateModuel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+class RotateModuel : MonoBehaviour
+{
+    public float speed = 10;
+    [SerializeField] private bool isRotate = false;
+
+    private void Update()
+    {
+        transform.RotateAround(
+            Vector3.zero,               // 회전할 대상의 포지션
+            Vector3.forward,   // 회전할 대상 축
+            speed);                     // 회전 속도
+    }
+
+
+
+}
+   
+
