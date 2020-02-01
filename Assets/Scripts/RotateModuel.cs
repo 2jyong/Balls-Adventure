4903012ff6d3a42251d8d4b650c992907c588ad8
2jyong <03jjjang@naver.com>
2020-02-01 15:34:35 +0900

    Balls-Adventure_02

--- Assets/Scripts/RotateModuel.cs
+++ Assets/Scripts/RotateModuel.cs
@@ -13,13 +13,33 @@ class RotateModuel : MonoBehaviour
 
     public float speed = 10;
     [SerializeField] private bool isRotate = false;
+    public Transform target = null;
+    public Vector3 point = Vector3.zero;
+    public Vector3 axis = Vector3.zero;
 
     private void Update()
     {
-        transform.RotateAround(
-            Vector3.zero,               // 회전할 대상의 포지션
-            Vector3.forward,   // 회전할 대상 축
-            speed);                     // 회전 속도
+        if (!isRotate) return;
+
+        if (target != null)
+        {
+            // 타겟이 비어있지 않으면
+
+            transform.RotateAround(target.localPosition, axis, speed);
+            // 나는 해당 축으로 회전한다. 
+            // target 의 localPosition 의 위치를 기준으로
+            // axis 에 speed 값을 곱한 값으로 회전한다
+
+
+        }
+        else if (point != Vector3.zero)
+        {       
+            transform.RotateAround(point, axis, speed);
+        }
+        else
+        {
+            transform.Rotate(axis, speed);
+        }
     }
 
 
